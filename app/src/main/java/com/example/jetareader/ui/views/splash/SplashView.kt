a986360e2d19b6004e69828814e83e033add678a
Dilermando Augusto <di_ler@hotmail.com>
2024-04-05 11:44:18 -0300

    217. Animating the Splash Screen

--- app/src/main/java/com/example/jetareader/ui/views/splash/SplashView.kt
+++ app/src/main/java/com/example/jetareader/ui/views/splash/SplashView.kt
@@ -1,5 +1,8 @@
 package com.example.jetareader.ui.views.splash
 
+import android.view.animation.OvershootInterpolator
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -10,20 +13,36 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.example.jetareader.R
+import kotlinx.coroutines.delay
 
 @Composable
 fun SplashView(navController: NavHostController) {
+    val scale = remember { Animatable(0f) }
+    LaunchedEffect(key1 = true) {
+        scale.animateTo(
+            targetValue = 1f,
+            animationSpec = tween(durationMillis = 800, easing = {
+                OvershootInterpolator(8f).getInterpolation(it)
+            })
+        )
+        delay(2000)
+    }
+
     Surface(
         modifier = Modifier
             .padding(15.dp)
-            .size(330.dp),
+            .size(330.dp)
+            .scale(scale.value),
         shape = CircleShape,
         color = Color.White,
         border = BorderStroke(width = 2.dp, color = Color.LightGray)
