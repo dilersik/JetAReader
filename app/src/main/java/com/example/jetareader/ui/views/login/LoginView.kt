9236ae559bdcdc784219acafb05f99107e1e6a0f
Dilermando Augusto <di_ler@hotmail.com>
2024-04-10 11:56:26 -0300

    223. Finishing Login and Create Account Logic

--- app/src/main/java/com/example/jetareader/ui/views/login/LoginView.kt
+++ app/src/main/java/com/example/jetareader/ui/views/login/LoginView.kt
@@ -1,18 +1,17 @@
 package com.example.jetareader.ui.views.login
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -26,11 +25,9 @@ import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import com.example.jetareader.R
@@ -42,6 +39,7 @@ import com.example.jetareader.ui.widgets.SubmitButton
 @Composable
 fun LoginView(navController: NavHostController) {
     val viewModel: LoginViewModel = hiltViewModel()
+    val showLoginForm = rememberSaveable { mutableStateOf(true) }
 
     Surface(modifier = Modifier.fillMaxSize()) {
         Column(
@@ -49,15 +47,24 @@ fun LoginView(navController: NavHostController) {
             verticalArrangement = Arrangement.Center
         ) {
             LogoWidget()
-            Form(viewModel = viewModel, loading = false, onDone = { email, pwd ->
+            UserForm(
+                viewModel = viewModel,
+                loading = false,
+                showLoginForm = showLoginForm,
+                onDone = { email, pwd ->
 
-            })
+                })
         }
     }
 }
 
 @Composable
-private fun Form(viewModel: LoginViewModel, loading: Boolean, onDone: (String, String) -> Unit) {
+private fun UserForm(
+    viewModel: LoginViewModel,
+    loading: Boolean,
+    showLoginForm: MutableState<Boolean>,
+    onDone: (String, String) -> Unit
+) {
     val emailState = rememberSaveable { mutableStateOf("") }
     val passwordState = rememberSaveable { mutableStateOf("") }
     val passwordVisibilityState = rememberSaveable { mutableStateOf(false) }
@@ -65,6 +72,19 @@ private fun Form(viewModel: LoginViewModel, loading: Boolean, onDone: (String, S
     val keyboardController = LocalSoftwareKeyboardController.current
     val validState = remember(emailState.value, passwordState.value) {
         viewModel.validateForm(emailState.value, passwordState.value)
+    }
+    val message = if (showLoginForm.value) {
+        Triple(
+            stringResource(R.string.login_btn),
+            stringResource(R.string.new_user_lbl),
+            stringResource(R.string.sign_up_lbl)
+        )
+    } else {
+        Triple(
+            stringResource(R.string.create_account_btn),
+            stringResource(R.string.already_have_an_account_link),
+            stringResource(R.string.login_link)
+        )
     }
 
     Column(
@@ -74,6 +94,14 @@ private fun Form(viewModel: LoginViewModel, loading: Boolean, onDone: (String, S
             .verticalScroll(rememberScrollState()),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        if (!showLoginForm.value) {
+            Text(
+                text = stringResource(R.string.create_account_info),
+                style = MaterialTheme.typography.bodyMedium,
+                textAlign = TextAlign.Center
+            )
+        }
+
         EmailInput(
             state = emailState,
             enabled = !loading,
@@ -94,12 +122,29 @@ private fun Form(viewModel: LoginViewModel, loading: Boolean, onDone: (String, S
         )
 
         SubmitButton(
-            text = stringResource(R.string.login_btn),
+            text = message.first,
             enabled = !loading && validState,
             loading = loading,
             onClick = {
                 onDone(emailState.value, passwordState.value)
+                keyboardController?.hide()
             }
         )
+
+        Row(
+            modifier = Modifier.padding(16.dp),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(text = message.second)
+            Text(
+                text = message.third,
+                modifier = Modifier
+                    .clickable { showLoginForm.value = !showLoginForm.value }
+                    .padding(5.dp),
+                fontWeight = FontWeight.Bold,
+                color = MaterialTheme.colorScheme.secondary
+            )
+        }
     }
 }
