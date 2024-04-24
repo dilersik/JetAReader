d4856f9008e932c7b0925d24e6b3de1721501529
Dilermando Augusto <di_ler@hotmail.com>
2024-04-24 10:24:28 -0300

    270. Adding the Top Part of the Book Update Screen

--- app/src/main/java/com/example/jetareader/ui/views/book_update/BookUpdateView.kt
+++ app/src/main/java/com/example/jetareader/ui/views/book_update/BookUpdateView.kt
@@ -1,22 +1,44 @@
 package com.example.jetareader.ui.views.book_update
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.ScrollState
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavHostController
+import coil.compose.rememberAsyncImagePainter
+import coil.request.ImageRequest
+import coil.size.Size
 import com.example.jetareader.R
 import com.example.jetareader.ui.views.book_update.BookUpdateViewConstants.PARAM
 import com.example.jetareader.ui.views.home.HomeViewModel
 import com.example.jetareader.ui.widgets.AppBarWidget
 
 @Composable
-fun BookUpdateView(navController: NavHostController, navBackStackEntry: NavBackStackEntry) {
+fun BookUpdateView(
+    navController: NavHostController,
+    navBackStackEntry: NavBackStackEntry,
+) {
     val bookId = navBackStackEntry.arguments?.getString(PARAM) ?: ""
     val viewModel: HomeViewModel = hiltViewModel()
     val book = viewModel.getBookById(bookId)
@@ -27,13 +49,62 @@ fun BookUpdateView(navController: NavHostController, navBackStackEntry: NavBackS
                 titleResId = R.string.book_update_title,
                 navController = navController,
             )
-        }
+        },
     ) { padding ->
         Column(
-            modifier = Modifier.padding(padding),
-            horizontalAlignment = Alignment.CenterHorizontally
+            modifier = Modifier
+                .padding(padding)
+                .verticalScroll(ScrollState(0)),
+            horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-            Text(text = book?.title.toString())
+            Row {
+                Card(
+                    modifier = Modifier
+                        .clip(RoundedCornerShape(20.dp))
+                        .padding(start = 4.dp, end = 4.dp, top = 4.dp, bottom = 8.dp)
+                        .clickable {},
+                    colors = CardDefaults.cardColors(containerColor = Color.White),
+                    elevation = CardDefaults.cardElevation(8.dp),
+                ) {
+                    Row {
+                        val painter = rememberAsyncImagePainter(
+                            model = ImageRequest.Builder(LocalContext.current)
+                                .data(book?.photoUrl).crossfade(true)
+                                .size(Size.ORIGINAL) // Important in order to work with verticalScroll
+                                .build()
+                        )
+                        Image(
+                            painter = painter,
+                            contentDescription = "",
+                            modifier = Modifier.height(140.dp),
+                        )
+                        Column(modifier = Modifier.padding(10.dp)) {
+                            Text(
+                                text = book?.title.toString(),
+                                style = MaterialTheme.typography.titleLarge,
+                                fontWeight = FontWeight.SemiBold,
+                                maxLines = 2,
+                            )
+                            Text(
+                                text = stringResource(
+                                    id = R.string.book_author_lbl,
+                                    book?.authors.toString(),
+                                ),
+                                modifier = Modifier.padding(top = 10.dp),
+                                style = MaterialTheme.typography.bodyLarge,
+                                maxLines = 2,
+                            )
+                            Text(
+                                text = stringResource(
+                                    id = R.string.book_date_lbl,
+                                    book?.publishedDate.toString(),
+                                ),
+                                style = MaterialTheme.typography.bodyLarge,
+                            )
+                        }
+                    }
+                }
+            }
         }
     }
 }
